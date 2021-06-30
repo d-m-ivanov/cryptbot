098510e4df75da915392177bd4c8e1cea9d4a2e8
d-m-ivanov <ivanofdm@gmail.com>
2021-06-30 20:38:10 +0700

    Simple command line interface added. Also little changes in backtester.py and binanceclient.py for correct work with files. Now paths for files work correctly! Typos in bot.py corrected. Simple command line interface improved. requirements.txt added

--- bot.py
+++ bot.py
@@ -18,18 +18,18 @@ def bot_help():
 
 def back_test_start():
     print("***** Start backtesting! *****")
-    base_asset = input("Pleas enter base asset (for example 'btc'): ").upper()
+    base_asset = input("Please enter base asset (for example 'USD'): ").upper()
     print("available candle intervals: 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M\n"
           "m -> minutes; h -> hours; d -> days; w -> weeks; M -> months")
     candle_interval = input("Please enter candle interval: ")
     short_term, long_term = input("Please enter short and long terms for moving average "
                                   "in form 'short_term long_term': ").split(" ")
-    trading_capital = float(input("Please enter a part of your capital that going to used in trading\n"
+    trading_capital = float(input("Please enter a part of your capital that going to be used in trading\n"
                                   "(example - 0.2): "))
-    start_month, start_day, start_year = input("Please enter start day of back testing "
-                                               "in format Mount Day Year: ").split(" ")
-    end_month, end_day, end_year = input("Please enter end day of back testing "
-                                         "in format Mount Day Year: ").split(" ")
+    start_year, start_month, start_day  = input("Please enter start day of back testing "
+                                                "in format yyyy-mm-dd: ").split("-")
+    end_year, end_month, end_day = input("Please enter end day of back testing "
+                                         "in format yyyy-mm-dd: ").split("-")
     start_date = datetime(year=int(start_year), month=int(start_month), day=int(start_day))
     end_date = datetime(year=int(end_year), month=int(end_month), day=int(end_day))
     quote_asset_amount = float(input("Please enter how much quote asset you have for back test: "))
@@ -47,20 +47,34 @@ def back_test_start():
 
 def test_trading_start():
     print("***** Trading on spot testnet will be executed! *****")
-    mode = "test"
-    client = initialize_client(mode)
-    strategy = initialize_strategy()
-    bot_interface = StartStrategy(client=client, strategy=strategy, mode="TEST")
-    bot_interface.start()
+    while True:
+        confirm = input("Do you want to continue and start real trading? [y/n]: ").lower()
+        if confirm == "y":
+            mode = "test"
+            client = initialize_client(mode)
+            strategy = initialize_strategy()
+            bot_interface = StartStrategy(client=client, strategy=strategy, mode="TEST")
+            bot_interface.start()
+        elif confirm == "n":
+            main()
+        else:
+            print("Try to print 'y' or 'n'")
 
 
 def trading_start():
     print("***** Caution! Real trading will be executed! *****")
-    mode = "prod"
-    client = initialize_client(mode)
-    strategy = initialize_strategy()
-    bot_interface = StartStrategy(client=client, strategy=strategy, mode="LIVE")
-    bot_interface.start()
+    while True:
+        confirm = input("Do you want to continue and start real trading? [y/n]: ").lower()
+        if confirm == "y":
+            mode = "prod"
+            client = initialize_client(mode)
+            strategy = initialize_strategy()
+            bot_interface = StartStrategy(client=client, strategy=strategy, mode="LIVE")
+            bot_interface.start()
+        elif confirm == "n":
+            main()
+        else:
+            print("Try to print 'y' or 'n'")
 
 
 def initialize_strategy() -> SMAStrategy:
@@ -70,9 +84,9 @@ def initialize_strategy() -> SMAStrategy:
     candle_interval = input("Please enter candle interval: ")
     short_term, long_term = input("Please enter short and long terms for moving average "
                                   "in form 'short_term long_term': ").split(" ")
-    trading_capital = float(input("Please enter a part of your capital that going to used in trading\n"
+    trading_capital = float(input("Please enter a fraction of your capital that going to used in trading\n"
                                   "(example - 0.2): "))
-    losses = float(input("Please enter part of capital you accept to lose: "))
+    losses = float(input("Please enter fraction of capital you accept to lose: "))
     short_term_int = int(short_term)
     long_term_int = int(long_term)
     return SMAStrategy(short_term=short_term_int, long_term=long_term_int,
@@ -84,15 +98,15 @@ def initialize_strategy() -> SMAStrategy:
 def initialize_client(mode="test") -> BinanceAPIClient:
     print("Initializing Binance API Client")
     if mode == "prod":
-        api_key = input("Pleas enter your Binance API KEY: ")
-        secret_key = input("Pleas enter your Binance SECRET KEY: ")
+        api_key = input("Please enter your Binance API KEY: ")
+        secret_key = input("Please enter your Binance SECRET KEY: ")
     elif mode == "test":
-        api_key = input("Pleas enter your Binance API KEY for spot testnet: ")
-        secret_key = input("Pleas enter your Binance SECRET KEY for spot testnet: ")
+        api_key = input("Please enter your Binance API KEY for spot testnet: ")
+        secret_key = input("Please enter your Binance SECRET KEY for spot testnet: ")
     else:
         api_key = ""
         secret_key = ""
-    base_asset = input("Pleas enter base asset (for example 'btc'): ").upper()
+    base_asset = input("Please enter base asset (for example 'btc'): ").upper()
     return BinanceAPIClient(base_asset=base_asset, quote_asset="USDT",
                             api_key=api_key, secret_key=secret_key, mode=mode)
 
@@ -107,7 +121,7 @@ def command_handler(command: str):
         trading_start()
         return True
     elif command == "test":
-        trading_start()
+        test_trading_start()
         return True
     elif command == "back_test":
         back_test_start()
@@ -125,4 +139,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        bot_help()
+        main()
+    except Exception as e:
+        print("Error: ", e)
+        main()
